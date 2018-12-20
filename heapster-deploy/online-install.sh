2c2204985ed2f5e074cfb1a10bf50d023e87b697
jevic <jie_071218@163.com>
2018-12-20 11:03:06 +0800

    online install

---
+++ heapster-deploy/online-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/rbac/heapster-rbac.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/grafana.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/heapster.yaml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/influxdb.yaml
